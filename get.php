502c8d04e6fb22c3ae2c5c4dad6905ac29a9459f
MuhBayu <bnugraha00@hotmail.com>
2017-07-29 14:23:46 +0700

    Update get.php

--- get.php
+++ get.php
@@ -11,103 +11,104 @@ if(isset($_REQUEST['you'])) {
 $data = [
 	[
 		'img' => 'aqua.jpg',
-		'desk' => 'waifu kamu adalah Aqua-sama.. si dewi sableng'
+		'desc' => 'waifu kamu adalah Aqua-sama.. si dewi sableng'
 	],
 	[
 		'img' => 'askontlo.jpg',
-		'desk' => 'waifu kamu adalah Astolfo. Yang sabar aja gan!'
+		'desc' => 'waifu kamu adalah Astolfo. Yang sabar aja gan!'
 	],
 	[
 		'img' => 'chitoge.png',
-		'desk' => 'waifu kamu adalah Chitoge. Si the best girl! dibanding yang sirambut hitam'
+		'desc' => 'waifu kamu adalah Chitoge. Si the best girl! dibanding yang sirambut hitam'
 	],
 	[
 		'img' => 'emilia.png',
-		'desk' => 'waifu kamu adalah Emilia-tan. Ini dabest girl gan! dibanding sirambut biru'
+		'desc' => 'waifu kamu adalah Emilia-tan. Ini dabest girl gan! dibanding sirambut biru'
 	],
 	[
 		'img' => 'julie.jpg',
-		'desk' => 'waifu kamu adalah Julie. ini mantap! bisa sekamar di asrama'
+		'desc' => 'waifu kamu adalah Julie. ini mantap! bisa sekamar di asrama'
 	],
 	[
 		'img' => 'kaori.jpg',
-		'desk' => 'waifu kamu adalah Kaori. Yang sabar aja gan! waifumu dah wafat mz..'
+		'desc' => 'waifu kamu adalah Kaori. Yang sabar aja gan! waifumu dah wafat mz..'
 	],
 	[
 		'img' => 'kotori.jpeg',
-		'desk' => 'waifu kamu adalah Kotori-chan. waifumu idol mz..'
+		'desc' => 'waifu kamu adalah Kotori-chan. waifumu idol mz..'
 	],
 	[
 		'img' => 'kurumi.jpg',
-		'desk' => 'waifu kamu adalah Kurumi. Wadoo kelar hidup lu bisa zafkiel juga'
+		'desc' => 'waifu kamu adalah Kurumi. Wadoo kelar hidup lu bisa zafkiel juga'
 	],
 	[
 		'img' => 'nico.jpg',
-		'desk' => 'waifu kamu adalah Nico. yang sabar aja gan! waifumu tydac berisi mz..'
+		'desc' => 'waifu kamu adalah Nico. yang sabar aja gan! waifumu tydac berisi mz..'
 	],
 	[
 		'img' => 'onodera.jpg',
-		'desk' => 'waifu kamu adalah Onodera. Yang sabar aja gan! Tetap siblonde selalu dihati'
+		'desc' => 'waifu kamu adalah Onodera. Yang sabar aja gan! Tetap siblonde selalu dihati'
 	],
 	[
 		'img' => 'pico.jpg',
-		'desk' => 'waifu kamu adalah Pico. Ini salah satu waifu terbaik yang ada di list waifu disini. kamu sangat beruntung mz..'
+		'desc' => 'waifu kamu adalah Pico. Ini salah satu waifu terbaik yang ada di list waifu disini. kamu sangat beruntung mz..'
 	],
 	[
 		'img' => 'rem.png',
-		'desk' => 'waifu kamu adalah Rem. waifumu latjur mz..'
+		'desc' => 'waifu kamu adalah Rem. waifumu latjur mz..'
 	],
 	[
 		'img' => 'asuna.png',
-		'desk' => 'waifu kamu adalah Asuna. No comment.. pokoknya da best'
+		'desc' => 'waifu kamu adalah Asuna. No comment.. pokoknya da best'
 	],
 	[
 		'img' => 'kou.jpg',
-		'desk' => 'waifu kamu adalah Kou. waifumu jago buat gem mz..'
+		'desc' => 'waifu kamu adalah Kou. waifumu jago buat gem mz..'
 	],
 	[
 		'img' => 'maki.png',
-		'desk' => 'waifu kamu adalah Maki. waifumu idol tsundere mz..'
+		'desc' => 'waifu kamu adalah Maki. waifumu idol tsundere mz..'
 	],
 	[
 		'img' => 'saber.png',
-		'desk' => 'waifu kamu adalah Saber. waifumu jago maen pedang mz..'
+		'desc' => 'waifu kamu adalah Saber. waifumu jago maen pedang mz..'
 	],
 	[
 		'img' => 'sena.jpg',
-		'desk' => 'waifu kamu adalah Sena. Wadoo.. no comment oppainya gan!'
+		'desc' => 'waifu kamu adalah Sena. Wadoo.. no comment oppainya gan!'
 	],
 	[
 		'img' => 'shinobu.jpeg',
-		'desk' => 'waifu kamu adalah Shinobu. Dih.. tercyduck'
+		'desc' => 'waifu kamu adalah Shinobu. Dih.. tercyduck'
 	],
 	[
 		'img' => 'tohka.jpg',
-		'desk' => 'waifu kamu adalah Tohka. Polos bisa diena2 mz..'
+		'desc' => 'waifu kamu adalah Tohka. Polos bisa diena2 mz..'
 	],
 	[
 		'img' => 'tomoe.jpg',
-		'desk' => 'waifu kamu adalah Tachibana Tomoe. waifumu sadis mz.. milf rasa loli'
+		'desc' => 'waifu kamu adalah Tachibana Tomoe. waifumu sadis mz.. milf rasa loli'
 	],
 	[
 		'img' => 'totsuka.png',
-		'desk' => 'waifu kamu adalah Totsuka. Memikat banyak hati pria'
+		'desc' => 'waifu kamu adalah Totsuka. Memikat banyak hati pria'
 	],
 	[
 		'img' => 'yoshiko.jpg',
-		'desk' => 'waifu kamu adalah Yoshiko. Enak nih.. bisa kasih pisang tiap hari!'
+		'desc' => 'waifu kamu adalah Yoshiko. Enak nih.. bisa kasih pisang tiap hari!'
 	],
 	[
 		'img' => 'ueno.png',
-		'desk' => 'waifu kamu adalah Ueno. Segeralah ganti waifumu..'
+		'desc' => 'waifu kamu adalah Ueno. Segeralah ganti waifumu..'
 	],
 	[
 		'img' => 'katou.jpg',
-		'desk' => 'waifu kamu adalah Katou Megumi. Da best girl!'
+		'desc' => 'waifu kamu adalah Katou Megumi. Da best girl!'
 	]
 ];
-$rand = array_rand($data,1);
-$pict = PATH_IMG.$data[$rand]['img'];
-$title = $nama . ' '.$data[$rand]['desk'];
-$desk = '<strong>'.$nama . '</strong> ' . $data[$rand]['desk'];
+$rand 	= array_rand($data,1);
+$result = $data[$rand];
+$pict 	= PATH_IMG . $result['img'];
+$title 	= $nama . ' ' . $result['desc'];
+$desc 	= '<strong>'.$nama . '</strong> ' . $result['desc'];
 ?>
